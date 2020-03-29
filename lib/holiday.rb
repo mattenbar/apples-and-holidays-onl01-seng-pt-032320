59a90b3404dfd2120c5dff8ecb07304daaf85dd9
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 21:04:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts seasons
     holidays.each do |holidays, supplies|
-      puts "#{holidays}: #{supplies.join(" ")"
+      puts "#{holidays}: #{supplies.join(" ")}"
     end
   end
 end
