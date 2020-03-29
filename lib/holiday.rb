b47583e09748c6b93be4abb54c8b9a7d1669ce12
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 23:20:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
       holiday.map do |holiday, item|
         holiday if item.include?("BBQ")
+        holiday.flatten.compact
       end
     end
   end
