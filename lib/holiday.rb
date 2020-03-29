f7b68710bc39c23527af75c15996b1f26160c56d
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 19:53:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |seasons, holidays|
     if seasons == :winter
-      holiday.each do |supplies|
+      holiday.map do |supplies|
         supplies << supply
       end
     end
