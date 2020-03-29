7a7236be522bd1da1ff40d3518695b9178062c08
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 21:09:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
-    puts seasons.to_s.captalize!
+    puts seasons.to_s
     holidays.each do |holidays, supplies|
       puts "#{holidays.to_s.capitalize}: #{supplies.join(" ")}"
     end
